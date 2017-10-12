6161de7392ba5e25ff9f22b389a97ccdab3764bb
kevinvud <kevinvud@gmail.com>
2017-10-12 08:04:04 -0700

    Initial Commit

---
+++ Social App/SecondViewController.swift
@@ -0,0 +1,25 @@
+//
+//  SecondViewController.swift
+//  Social App
+//
+//  Created by PoGo on 10/12/17.
+//  Copyright © 2017 PoGo. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
